fc0a622c33c99b555cb6aa742dcc958938c8b48f
Neology92 <oskarlegner@gmail.com>
2020-07-01 18:38:35 +0200

    Remove force-ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,15 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :debt_manager, DebtManagerWeb.Endpoint,
-  url: [host: "debt-manager-app.herokuapp.com", port: 443],
-  force_ssl: [hsts: true],
-  https: [
-    port: 443,
-    cipher_suite: :strong,
-    keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-    certfile: System.get_env("SOME_APP_SSL_CERT_PATH"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
+  url: [host: "debt-manager-app.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
